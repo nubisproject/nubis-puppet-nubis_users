7d298b1e0a6fd10cd3982f836357b0c6694b4127
limed <limed@mozilla.com>
2016-09-01 10:02:36 -0700

    If managehome is set to true then create home directories, if not don't bother to do anything

--- manifests/user.pp
+++ manifests/user.pp
@@ -35,27 +35,29 @@ define nubis_users::user(
     shell      => $shell,
   }
 
-  # create home directory and generate ssh keys
-  file { "/home/${username}":
-    ensure => $directory_ensure,
-    owner  => $name,
-    group  => $name,
-    mode   => '0700',
-  }
-  file { "/home/${username}/.ssh":
-    ensure  => $directory_ensure,
-    owner   => $name,
-    group   => $name,
-    mode    => '0700',
-    require => File["/home/${username}"],
-  }
-  file { "/home/${username}/.ssh/authorized_keys":
-    ensure  => $file_ensure,
-    owner   => $name,
-    group   => $name,
-    mode    => '0600',
-    content => template('nubis_users/authorized_keys.erb'),
-    require => [ User[$username], File["/home/${username}/.ssh"] ],
+  if $managehome {
+    # create home directory and generate ssh keys
+    file { "/home/${username}":
+      ensure => $directory_ensure,
+      owner  => $name,
+      group  => $name,
+      mode   => '0700',
+    }
+    file { "/home/${username}/.ssh":
+      ensure  => $directory_ensure,
+      owner   => $name,
+      group   => $name,
+      mode    => '0700',
+      require => File["/home/${username}"],
+    }
+    file { "/home/${username}/.ssh/authorized_keys":
+      ensure  => $file_ensure,
+      owner   => $name,
+      group   => $name,
+      mode    => '0600',
+      content => template('nubis_users/authorized_keys.erb'),
+      require => [ User[$username], File["/home/${username}/.ssh"] ],
+    }
   }
 
   # ensure subdirs are purged if ensure is set to absent
