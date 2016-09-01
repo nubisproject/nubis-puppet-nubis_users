53039e2db286ea2fb4ebea31ca267896798cc52a
limed <limed@mozilla.com>
2016-09-01 10:35:37 -0700

    Add some default parameters

--- manifests/managed.pp
+++ manifests/managed.pp
@@ -1,5 +1,10 @@
-class nubis_users::managed{
+class nubis_users::managed(
+  $ensure     = 'present',
+  $managehome = false,
+  $shell    = '/sbin/nologin',
+){
 
+  # anyone who doesn't belong gets purged
   resources { 'user':
     purge => true,
   }
@@ -7,6 +12,6 @@ class nubis_users::managed{
   # we want to make sure that these few users are actually
   # not purged so we just make sure they exist
   $managed = hiera_hash('nubis_users::managed', {})
-  create_resources('nubis_users::user', $managed)
+  create_resources('nubis_users::user', $managed, { 'managehome' => $managehome, 'shell' =>  $shell })
 
 }
