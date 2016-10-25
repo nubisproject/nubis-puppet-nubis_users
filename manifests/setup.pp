1341db41e12d8f3381b49d6ef1f8b80c322f3576
limed <limed@mozilla.com>
2016-10-24 17:18:51 -0700

    This commit attempts to change the way this module, it will now templatize the confd tmpl and toml files. This will allow us to have more granular control on the node levels to grant users for specific ldap groups.
    Doing this will move all the confd files from nubis-base to here

---
+++ manifests/setup.pp
@@ -0,0 +1,48 @@
+# This class sets up the confd and hiera files required
+#
+class nubis_users::setup(
+  $global_admins  = [ 'nubis_global_admins' ],
+  $sudo_users     = [],
+  $nubis_users    = [],
+){
+
+  file { '/etc/puppet/yaml':
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+  }
+
+  file { '/etc/puppet/hiera.yaml':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/nubis_users/hiera.yaml',
+  }
+
+  file { '/etc/puppet/yaml/nubis_users':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    recurse => true,
+    source  => 'puppet:///modules/nubis_users/hiera',
+  }
+
+  file { '/etc/confd/conf.d/nubis-users.toml':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('nubis_users/confd/nubis-users.toml.erb'),
+  }
+
+  file { '/etc/confd/templates/nubis-users.tmpl':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('nubis_users/confd/nubis-users.tmpl.erb'),
+    require => File['/etc/confd/conf.d/nubis-users.toml'],
+  }
+}
